ba8392a0cbdf766a9161f4cecdbb42a98f669ac1
weijie <865304121@qq.com>
2019-11-08 21:50:58 +0800

    first commit

---
+++ jd-webapp/commjx/src/components/HelloWorld.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+      <van-nav-bar
+              left-text="返回"
+              title="购物车"
+              left-arrow
+              @click-left="onClickLeft"
+      />
+
+    <van-card
+            num="2"
+            :key="i"
+            v-for="(p,i) in list"
+            :price="p.fontprize"
+            :desc="p.fonteqiu"
+            :title="p.fontphoto"
+            thumb="https://openfile.meizu.com/group1/M00/06/CC/Cgbj0VvINL-AGM20AAw4GirVtYA698.png@240x240.jpg"
+    >
+      <div slot="tags">
+        <van-tag plain type="danger">加价购</van-tag>
+        <van-tag plain type="danger">特价购</van-tag>
+      </div>
+      <div slot="footer">
+        <van-button size="mini">删除</van-button>
+        <van-button size="mini">添加</van-button>
+      </div>
+    </van-card>
+    <van-tabbar v-model="active">
+          <van-tabbar-item icon="home-o" @click-left="onClickLeft">首页</van-tabbar-item>
+          <van-tabbar-item icon="search">详情</van-tabbar-item>
+          <van-tabbar-item icon="friends-o">购物车</van-tabbar-item>
+          <van-tabbar-item icon="setting-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+    import { NavBar } from 'vant';
+  import { Card,Tag,Tabbar,TabbarItem } from 'vant';
+  import { button } from "vant"
+export default {
+  name: 'HelloWorld',
+  components:{
+      "van-card":Card,
+      "van-nav-bar":NavBar,
+      "van-tag":Tag,
+      "van-button":button,
+      "van-tabbar":Tabbar,
+      "van-tabbar-item":TabbarItem
+  },
+    data(){
+      return {
+          active:0,
+          list:[
+              {
+                  fonteqiu: "魅族 Note9",
+                  fontprize:"1199.00",
+                  fontphoto:"4800万|前置2000万|独家定制水滴设计全面屏|4000Ah大"
+              },{
+                  fonteqiu: "魅族 16s Pro",
+                  fontprize:"2199.00",
+                  fontphoto:"【限时3期免息】骁龙845 | 屏幕下指纹 | 前后2000万像素摄像头"
+              }
+              ],
+
+      }
+    },
+  props: {
+    msg: String
+  },
+    methods:{
+        onClickLeft() {
+            this.$router.replace('/list')
+        },
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ jd-webapp/commjx/src/components/loginlu.vue
@@ -0,0 +1,166 @@
+<template>
+
+    <div class="menuall">
+        <van-nav-bar
+                left-text="返回"
+                left-arrow
+                @click-left="onClickLeft"
+        />
+        <div class="menuList">
+            <ul>
+                <li @click="flag = true" style="border-right: 0.005rem solid #ccc;">
+                    {{list[0]}}
+                </li>
+                <li @click="flag = false">
+                    {{list[1]}}
+                </li>
+            </ul>
+        </div>
+
+            <div  v-if="flag" class="tabCon">
+                <input type="text" placeholder="手机号/Flyme账号">
+                <input type="password" placeholder="密码">
+                <p class="caa"><input type="text" placeholder="点击按钮进行验证"><van-loading type="spinner" color="#1989fa" /></p>
+                <p class="onlybag"><input type="checkbox">：记住登录状态<a>忘记密码?</a></p>
+                <button>登录</button>
+                <button style="background-color: white;color: black;margin-top: 0;border: none">注册</button>
+            </div>
+            <div  v-else class="tabCon">
+                <input type="text" maxlength="11" placeholder="手机号码" v-model="captche">
+                <button>登录</button>
+                <button style="background-color: white;color: black;margin-top: 0;border: none">注册</button>
+
+            </div>
+
+    </div>
+    
+</template>
+
+<script>
+    import { NavBar } from 'vant';
+    import {Loading} from  'vant'
+    export default {
+        name: "loginlu",
+        components:{
+            "van-loading":Loading,
+            "van-nav-bar":NavBar
+            },
+        data() {
+            return {
+                captche:"",
+                Toast:"",
+                sms:"",
+                username:"",
+                phone:"",
+                flag: true,
+                num: 0,
+                list: ["账号登录", "验证码登录"],
+            }
+        },
+        methods:{
+            onClickLeft() {
+                this.$router.replace('/list')
+            },
+        }
+    }
+</script>
+
+<style scoped>
+    .menuall{
+        width: 100%;
+        height: 5rem;
+        margin: 0.2rem auto;
+        background-color: #fff;
+
+    }
+    .active {
+        color: #fff;
+        background: #e74c3c;
+    }
+
+
+    .menuList {
+        width:  70%;
+        margin: 0 auto;
+        height: 0.6rem;
+        background-color: white;
+    }
+
+    ul {
+        width: 100%;
+        display: flex;
+        list-style: none;
+        padding: 0;
+        margin: 0;
+        color: black;
+        font-size: 0.16rem;
+        line-height: 0.6rem;
+
+    }
+
+    ul li {
+        width: 50%;
+        text-align: center;
+        border: none;
+        cursor: pointer;
+    }
+
+    .tabCon {
+        width: 90%;
+        margin: 0 auto;
+        padding: 0.2rem 0.2rem;
+        color: #999;
+        font-size: 0.14rem;
+        background-color: #fff;
+    }
+    .tabCon input{
+        width:100%;
+        margin: 0 auto;
+        height: 0.46rem;
+        text-indent: 0.12rem;
+        margin-bottom: 0.2rem;
+        border: 0.005rem solid #ccc;
+    }
+    .caa{
+        width: 100%;
+        font-size: 0.14rem;
+        margin: 0 0;
+        display: flex;
+    }
+    .caa input{
+        width: 74%;
+        margin: 0 0;
+    }
+    .van-loading__spinner--spinner {
+        margin-top:0.08rem;
+        margin-left: 0.2rem;
+    }
+    .onlybag{
+        width: 100%;
+    }
+
+    .onlybag  input{
+        width: 0.2rem;
+        height:0.2rem;
+        float: left;
+    }
+    .onlybag a{
+        float: right;
+        color: #00C3F5;
+    }
+    .tabCon button{
+         outline: none;
+         border-radius: 0.04rem;
+         width: 100%;
+         font-size: 0.14rem;
+         margin-top: 0.2rem;
+         color: white;
+         height: 0.5rem;
+         background-color: #198DED;;
+     }
+    .van-hairline--bottom::after{
+        border-bottom-width:0 ;
+    }
+
+
+</style>
